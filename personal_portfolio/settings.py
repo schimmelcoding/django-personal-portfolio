071bf210ab41cd88c651523a10ae1315e5c29fa8
Bruce Schimmel <bruce.schimmel@gmail.com>
2024-01-16 22:44:01 -0600

    fixing typo in db password

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'posgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
