30d473ab1f53a3a9b5e0695e2ebd5a0e1eaa20ae
Bruce Schimmel <bruce.schimmel@gmail.com>
2024-01-18 00:48:38 -0600

    adjusted file structure

--- ags_demo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AgsDemoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ags_demo'
--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
-# Create your views here.
-def all_blogs(request):
-    posts = Post.objects.order_by('-date')
-    return render(request, 'blog/all_blogs.html', {'posts':posts})
-def detail(request, blog_id):
-    post = get_object_or_404(Post, pk=blog_id)
-    return render(request, 'blog/detail.html', {'post':post})
